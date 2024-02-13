e61933055334099b489f79d2a8509f780f1da33e
feranmi adewale adeniji <feranmi@kickgame.co.uk>
2024-02-13 02:29:49 +0100

    fix deployment issues

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-import { ToastContainer } from 'react-toastify';
+// import { ToastContainer } from 'react-toastify';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -16,8 +16,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    <ToastContainer 
-      position="bottom-center"
-    />
+
   </Suspense>,
 )
